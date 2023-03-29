550f6a6a1034c7a80dc3e0d662b80961479aef96
yoongi <lolhongy3@gmail.com>
2023-03-29 22:25:05 +1100

    complet exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if(times < 0){
+        return "ERROR";
+    }
+    let words = '';
+    for(let i=1; i<=times; i++){
+        words += word;
+    }
+    return words;
 };
 
 // Do not edit below this line
