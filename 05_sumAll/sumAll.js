798fd6c4dd0e9ba7646ec69da0212ca301b82274
yoongi <lolhongy3@gmail.com>
2023-03-30 19:07:22 +1100

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if(!Number.isInteger(x) || !Number.isInteger(y) || x<0 || y<0){
+        return "ERROR"
+    }
+    let smallerNum = x;
+    let largerNum = y;
+    if(x>y){
+        smallerNum = y;
+        largerNum = x;
+    }
+    let sum = 0;
+    for(let i=smallerNum; i<=largerNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
