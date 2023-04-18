6deb26052be2e8daa89e12528730db315a3bf62f
yoongi <lolhongy3@gmail.com>
2023-04-19 02:09:42 +1000

    complete excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson)=>{
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        
+        return currentAge > oldestAge ? currentPerson : oldest
+    })
 };
+
+const getAge = function (birth, death){
+    if(!death){
+        death = 2023;
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
